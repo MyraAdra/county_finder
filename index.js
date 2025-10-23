61ceb7f2c2f192a375a0cc0c8004462487530f48
MyraAdra <myra7314@gmail.com>
2025-10-23 09:15:20 +0200

    updated files

--- index.js
+++ index.js
@@ -1,101 +1,80 @@
 let countries = [];
 
-
 function loadAPIData() {
-
-    fetch('https://restcountries.com/v3.1/all?fields=name,flags')
-        .then(response => response.json())
-        .then(json => {
-
-            json.forEach(country => {
-                //console.log(country);
-                let new_country ={name: country.name.common,
-                                  flag: country.flags.png}  
-
-                countries.push(new_country);                  
-        
-            });
-        })
-
+  fetch('https://restcountries.com/v3.1/all?fields=name,flags')
+    .then(response => response.json())
+    .then(json => {
+      json.forEach(country => {
+        let new_country = {
+          name: country.name.common,
+          flag: country.flags.png
+        };
+        countries.push(new_country);
+      });
+    });
 }
 
-loadAPIData()
-
+loadAPIData();
 
 function loadAllCountries() {
+  let country_ddl = document.getElementById("country_ddl");
+  let flagdiv = document.getElementById("flagdiv");
 
-    let country_ddl = document.getElementById("country_ddl");
+  // Clear previous results
+  country_ddl.innerHTML = "";
+  flagdiv.innerHTML = "";
 
-    countries.forEach(country => {
-
+  // Populate dropdown and flags
+  countries.forEach(country => {
     let country_option = document.createElement("option");
     country_option.innerHTML = country.name;
     country_ddl.appendChild(country_option);
 
-    })
-
-
-
-    let flagdiv = document.getElementById("flagdiv");
-
-    countries.forEach(country => {
-
     let country_image = document.createElement("img");
     country_image.src = country.flag;
-    country_image.className ='flag';
+    country_image.className = 'flag';
     flagdiv.appendChild(country_image);
 
     country_image.addEventListener("click", function() {
-            alert(country.name);
-        });
+      //alert(country.name);
+      displayCountryInfo(country.name);
+    });
+  });
+}
 
-    })
+function display_name() {
+  let country_ddl = document.getElementById("country_ddl");
+  let chosen_country = country_ddl.value;
+  let span_name = document.getElementById("span_name");
+  span_name.innerHTML = chosen_country;
+}
+
+function display_name2() {
+  let country_ddl = document.getElementById("country_ddl");
+  let chosen_country = country_ddl.value;
+
+  displayCountryInfo(chosen_country);
 
 }
 
-function display_name(){
+// New function: fetch more info on click
+function displayCountryInfo(selected_country) {
 
-    
-    let country_ddl = document.getElementById("country_ddl");
-    let chosen_country = country_ddl.value;
+  fetch(`https://restcountries.com/v3.1/name/${selected_country}`)
+    .then((response) => response.json())
+    .then((json) => {
+      let country_info = document.getElementById('country_info');
+      let name = json[0].name.common;
+      let capital = json[0].capital[0];
 
-    //let topddiv = document.getElementById("topdiv");
-    let span_name = document.getElementById("span_name");
-    span_name.innerHTML = chosen_country;
+      let population = json[0].population.toLocaleString();
+     
 
-    //alert(chosen_country);
-
-
-
+      // Display nicely formatted info
+      country_info.innerHTML = `
+        <h2>${name}</h2>
+        <p><b>Capital:</b> ${capital}</p>
+        <p><b>Population:</b> ${population}</p>
+      `;
+    });
 }
-    
-
-
-
-// fetch('https://restcountries.com/v3.1/all?fields=name,flags')
-//   .then(response => response.json())
-//   .then(json => {
-//     json.forEach(element => {
-//       countries.push({
-//         name: element.name.common,
-//         flag: element.flags.png,
-//       });
-//     });
-//   });
-
-
-// function loadAllCountries() {
-
-//     fetch('https://restcountries.com/v3.1/all?fields=name,flags')
-//         .then((response) => response.json())
-//         .then(json => 
-//             json.forEach((element) => {
-//                 countries.push({
-//                     name: element.name.common,
-//                      flag: element.flags.png,
-//                  });
-       
-//             });
-//         );
-
-// }
